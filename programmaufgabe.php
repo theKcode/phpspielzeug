cbb09bdfca34dc8cc09aba6e668faa77fe3ac736
max <akaminski@gmx.de>
2017-10-19 16:03:46 +0200

    javazweigc

--- programmaufgabe.php
+++ programmaufgabe.php
@@ -3,74 +3,42 @@
 <?php
 
 
-for($i = 1; $i < 200; $i++) {
+for($i = 1; $i <= 200; $i++) {
 
-
-
-// commit -a
-	$flag=false;
-    if($i % 2 == 0)
+	$j = $i;
+    if( ($i % 2) == 0)
 	{
-		echo "zwei\n";
+		$j = "zwei\n";
 		$flag=true;
 	}
 
-	if($i % 5 == 0)
+	if( ($i % 5) == 0)
 	{
-		echo "fuenf\n";
+		$j = "fuenf\n";
 		$flag=true;
 	}
 
-	if(!$flag){
-		echo "$i\n";
-	}
-
-	if($i % 7 == 0)
+	if( ($i % 7) == 0)
 	{
-		echo "sieben\n";
+		$j = "sieben\n";
 		$flag=true;	
 	}
 
-        if($i % 2 == 0 && $i % 5 == 0)
+        if( ($i %(2*5))  == 0)
         {
-                echo "zweifuenf\n";
+                $j = "zweifuenf\n";
                 $flag=true;
         }
 
-        if($i % 2 == 0 && $i % 7 == 0)
+        if( ($i %(2*7))  == 0)
         {
-                echo "zweisieben\n";
+                $j = "zweisieben\n";
                 $flag=true;
         }
 
-        if($i % 5 == 0 && $i % 7 == 0)
-        {
-                echo "fuenfsieben\n";
-                $flag=true;
-        }
-
-        if($i % 2 == 0 && $i % 5 == 0 && $i % 7 == 0)
-        {
-                echo "zweifuenfsieben\n";
-                $flag=true;
-
-        }
 
 
-
-/*
-	switch ($i  % 2 == 0) {
-    		case 2:
-        	echo "zwei\n";
-       		break;
-  	  case 5:
-      		echo "fünf\n";
-      		break;
-    	  case 7:
-     		echo "sieben";
-        	break;
-	}
-*/
+	echo "$j\n";
 }
 
 ?>
